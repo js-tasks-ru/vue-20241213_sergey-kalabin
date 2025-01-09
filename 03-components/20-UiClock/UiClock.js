88600a641a3798b10e79fb46ffa8c97f4f5e83df
sergeykalabin <sergey.kalabin@legalbet.com>
2025-01-09 22:38:47 +0300

    Решил 5 задач из раздела компонеты

--- 03-components/20-UiClock/UiClock.js
+++ 03-components/20-UiClock/UiClock.js
@@ -1,9 +1,31 @@
-import { defineComponent } from 'vue'
+import { defineComponent, ref, onMounted, onUnmounted } from 'vue'
 
 export default defineComponent({
   name: 'UiClock',
 
-  setup() {},
+  setup() {
+    const timeData = new Intl.DateTimeFormat(navigator.language, { timeStyle: 'medium' });
 
-  template: `<div class="clock">10:12:02</div>`,
+    const timeActual = ref('');
+
+    let intervalCount = 0;
+
+    onMounted(() => {
+      timeActual.value = timeData.format(new Date());
+
+      intervalCount = setInterval(() => {
+        timeActual.value = timeData.format(new Date());
+      }, 1000);
+    })
+
+    onUnmounted(() => {
+      clearInterval( intervalCount )
+    });
+
+    return {
+      timeActual,
+    }
+  },
+
+  template: `<div class="clock">{{ timeActual }}</div>`,
 })
